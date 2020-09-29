a7a20d3d565ff1173be05e5d66783b3a30c91c18
Dennis Sperber <dennissperber@live.nl>
2020-09-29 13:26:13 +0200

    added ability to send chat messages

---
+++ bugtracking-backend/bugtracking-server/src/main/java/com/rage/bugtrackingserver/controllers/ChatController.java
@@ -0,0 +1,17 @@
+package com.rage.bugtrackingserver.controllers;
+
+import com.rage.bugtrackingserver.models.Greeting;
+import com.rage.bugtrackingserver.models.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        return new Greeting(HtmlUtils.htmlEscape(message.getName()));
+    }
+}
