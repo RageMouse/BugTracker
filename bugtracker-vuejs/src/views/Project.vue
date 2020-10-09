8a8ec19ca27f9077e3ba1340497707cea96d4ba9
Dennis Sperber <dennissperber@live.nl>
2020-10-09 03:07:05 +0200

    Added Project view.

---
+++ bugtracker-vuejs/src/views/Project.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12"></v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Here are all the current projects.
+        </h1>
+
+        <v-banner class="subheading font-weight-regular">
+          Where projects comes to die.
+        </v-banner>
+      </v-col>
+    </v-row>
+    <v-row> </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Project",
+  components: {},
+};
+</script>
